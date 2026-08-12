27fd803e9437dcec1a9bcdc6bde970246829a41f
LinetteMi94 <Linakorneva94@mail.ru>
2026-08-12 23:30:41 +1000

    Add homework for OOP practice

---
+++ Monster.cs
@@ -0,0 +1,22 @@
+﻿namespace RPG_Game;
+
+/// <summary>
+/// Представляет монстра в игре.
+/// Наследует общие свойства и поведение от класса Character.
+/// </summary>
+public class Monster(string name, int health, int armor) : Character(name, health)
+{
+    private int Armor { get; } = armor;
+    
+    public override void TakeDamage(int damage)
+    {
+        int realDamage = damage - Armor;
+        if (realDamage > 0) base.TakeDamage(realDamage);
+    }
+
+    public override void DisplayCharacterStats()
+    {
+        base.DisplayCharacterStats();
+        Console.WriteLine($"Броня: {Armor}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace RPG_Game;
+
+internal static class Program
+{
+    static void Main(string[] args)
+    {
+        Hero hero1 = new Hero("Рокки", 100, 5,6);
+        Monster goblin = new Monster("Гоблин", 200, 40);
+        Monster rat = new Monster("Крыса", 50, 10);
+        hero1.TakeDamage(70);
+        goblin.TakeDamage(30);
+        hero1.Heal(78);
+        rat.TakeDamage(30);
+        rat.DisplayCharacterStats();
+        hero1.DisplayCharacterStats();
+        goblin.DisplayCharacterStats();
+    }
+}
